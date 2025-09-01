994733a26555c2729855321af29f83104fd8d4e0
Matheus Duca <theu.duka@gmail.com>
2025-08-31 22:39:11 -0300

    feat: gravidade e aceleração / desaceleração

--- Assets/_Game/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Game/_Scripts/Player/PlayerMovement.cs
@@ -1,43 +1,100 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// Responsável pela execução dos comportamentos relacionados a movimentação do player
-/// </summary>
-public class PlayerMovement : MonoBehaviour
+namespace Duca
 {
-    [Header("Parâmetros")]
-    [SerializeField] private float _moveSpeed;
-
-    [Header("Referências")]
-    [SerializeField] private CharacterController _characterController;
-    [SerializeField] private InputActionReference _moveAction;
-
-    // Não serializadas
-    private Vector2 _moveInput = Vector2.zero;
-
-    private void OnValidate()
+    /// <summary>
+    /// Responsável pela execução dos comportamentos relacionados a movimentação do player
+    /// </summary>
+    public class PlayerMovement : MonoBehaviour
     {
-        _characterController = GetComponent<CharacterController>();
+        [Header("Parâmetros Movimento")]
+        [SerializeField] private float _moveSpeed = 5f;
+        [SerializeField] private float _acceleration = 10f;
+        [SerializeField] private float _deceleration = 10f;
+
+        [Header("Parâmetros Pulo/Gravidade")]
+        [SerializeField] private float _jumpForce = 8f;
+        [SerializeField] private float _groundGravity = -2f;
+        [SerializeField] private float _gravityForce = -9.81f;
+
+        [Header("Referências")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private InputActionReference _moveAction;
+        [SerializeField] private InputActionReference _jumpAction;
+
+        // Não serializadas
+        private Vector2 _moveInput = Vector2.zero;
+        private Vector3 _curHorizontalSpeed = Vector3.zero;
+        private Vector3 _targetHorizontalSpeed = Vector3.zero;
+        private float _curVerticalSpeed;
+
+        private void OnValidate() => _characterController = GetComponent<CharacterController>();
+
+        private void OnEnable() => SetInputs(true);
+
+        private void OnDisable() => SetInputs(false);
+
+        private void Update()
+        {
+            ApplyMovement();
+            ApplyGravity();
+        }
+
+        #region Inputs
+
+        private void SetInputs(bool value)
+        {
+            if (value)
+            {
+                _moveAction.action.performed += GetMoveInput;
+                _moveAction.action.canceled += ResetMoveInput;
+                _jumpAction.action.performed += GetJumpInput;
+            }
+            else
+            {
+                _moveAction.action.performed -= GetMoveInput;
+                _moveAction.action.canceled -= ResetMoveInput;
+                _jumpAction.action.performed -= GetJumpInput;
+            }
+        }
+
+        private void GetMoveInput(InputAction.CallbackContext contextValue) => _moveInput = contextValue.ReadValue<Vector2>();
+        private void ResetMoveInput(InputAction.CallbackContext contextValue) => _moveInput = Vector2.zero;
+        private void GetJumpInput(InputAction.CallbackContext contextValue) => ApplyJump();
+
+        #endregion
+
+        #region Física
+
+        private void ApplyMovement()
+        {
+            _targetHorizontalSpeed = new Vector3(_moveInput.x, 0f, _moveInput.y) * _moveSpeed;
+
+            if (_targetHorizontalSpeed.magnitude > 0.1f)
+                _curHorizontalSpeed = Vector3.Lerp(_curHorizontalSpeed, _targetHorizontalSpeed, _acceleration * Time.deltaTime);
+            else
+                _curHorizontalSpeed = Vector3.Lerp(_curHorizontalSpeed, Vector3.zero, _deceleration * Time.deltaTime);
+
+            _characterController.Move(_curHorizontalSpeed * Time.deltaTime);
+        }
+
+        private void ApplyJump()
+        {
+            if (_characterController.isGrounded)
+                _curVerticalSpeed = _jumpForce;
+        }
+
+        private void ApplyGravity()
+        {
+            if (_characterController.isGrounded && _curVerticalSpeed < 0)
+                _curVerticalSpeed = _groundGravity;
+
+            _curVerticalSpeed += _gravityForce * Time.deltaTime;
+
+            _characterController.Move(Vector3.up * _curVerticalSpeed * Time.deltaTime);
+        }
+
+        #endregion
     }
-
-    private void Start()
-    {
-        _moveAction.action.performed += GetMoveInput;
-        _moveAction.action.canceled += ResetMoveInput;
-    }
-
-    private void Update() => ApplyMovement();
-
-    private void GetMoveInput(InputAction.CallbackContext contextValue)
-    {
-        _moveInput = contextValue.ReadValue<Vector2>();
-    }
-
-    private void ResetMoveInput(InputAction.CallbackContext contextValue)
-    {
-        _moveInput = Vector2.zero;
-    }
-
-    private void ApplyMovement() => _characterController.Move(new Vector3(_moveInput.x, 0f, _moveInput.y) * _moveSpeed * Time.deltaTime);
 }
--- Assets/_Game/_Scripts/SpellCaller/SpellCaller.cs
+++ Assets/_Game/_Scripts/SpellCaller/SpellCaller.cs
@@ -4,48 +4,51 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-/// <summary>
-/// Responsável pelo acionamento de spells via reconhecimento de voz
-/// </summary>
-public class SpellCaller : MonoBehaviour
+namespace Duca
 {
-    [Header("Referências")]
-    [SerializeField] private List<SpellData> _spellDatas;
-
-    // Não serializadas
-    private KeywordRecognizer _recognizer;
-    private Dictionary<string, Action> _spellDict = new();
-
-    private void Awake() => SetupSpells();
-
-    private void OnApplicationQuit()
+    /// <summary>
+    /// Responsável pelo acionamento de spells via reconhecimento de voz
+    /// </summary>
+    public class SpellCaller : MonoBehaviour
     {
-        if (_recognizer.IsRunning)
-            _recognizer.Stop();
-    }
+        [Header("Referências")]
+        [SerializeField] private List<SpellData> _spellDatas;
 
-    private void CheckSpell(PhraseRecognizedEventArgs speechValue) => _spellDict[speechValue.text].Invoke();
+        // Não serializadas
+        private KeywordRecognizer _recognizer;
+        private Dictionary<string, Action> _spellDict = new();
 
-    private void SetupSpells()
-    {
-        foreach (SpellData data in _spellDatas)
+        private void Awake() => SetupSpells();
+
+        private void OnApplicationQuit()
         {
-            if (_spellDict.ContainsKey(data.Name))
-            {
-                Debug.LogError("Spell já adicionada ao dicionário!");
-                return;
-            }
-
-            _spellDict.Add(data.Name, data.Cast);
+            if (_recognizer.IsRunning)
+                _recognizer.Stop();
         }
 
-        SetupRecognizer();
-    }
+        private void CheckSpell(PhraseRecognizedEventArgs speechValue) => _spellDict[speechValue.text].Invoke();
 
-    private void SetupRecognizer()
-    {
-        _recognizer = new KeywordRecognizer(_spellDict.Keys.ToArray());
-        _recognizer.OnPhraseRecognized += CheckSpell;
-        _recognizer.Start();
+        private void SetupSpells()
+        {
+            foreach (SpellData data in _spellDatas)
+            {
+                if (_spellDict.ContainsKey(data.Name))
+                {
+                    Debug.LogError("Spell já adicionada ao dicionário!");
+                    return;
+                }
+
+                _spellDict.Add(data.Name, data.Cast);
+            }
+
+            SetupRecognizer();
+        }
+
+        private void SetupRecognizer()
+        {
+            _recognizer = new KeywordRecognizer(_spellDict.Keys.ToArray());
+            _recognizer.OnPhraseRecognized += CheckSpell;
+            _recognizer.Start();
+        }
     }
 }
