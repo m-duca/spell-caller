879a844ff8fba01bb5992dfe12844fa2610aaa8c
Matheus Duca <theu.duka@gmail.com>
2025-09-30 22:09:51 -0300

    feat: feedback de dano

--- Assets/_Game/_Scripts/TrainingDummy/TrainingDummyCollision.cs
+++ Assets/_Game/_Scripts/TrainingDummy/TrainingDummyCollision.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 namespace SpellCaller
 {
     public class TrainingDummyCollision : MonoBehaviour, IDamageable
     {
-        [Header("Pârametros")]
+        [Header("Parâmetros")]
         [SerializeField] private int _startLife;
-        [SerializeField] private int _currentLife;
+        private int _currentLife;
+
+        [Header("Feedback de Dano")]
+        [SerializeField] private float _rotationDuration;
+        [SerializeField] private float _rotationAngle;
+        [SerializeField] private Color _damageColor;
+        [SerializeField] private float _colorFlashDuration;
 
         // Não serializadas
         private bool _canApplyContinuosDamage = true;
         private Animator _animator;
+        private bool _isDead;
+        private Renderer _renderer;
+        private Color _originalColor;
 
         private const string ANIM_PARAM_DAMAGED = "_damaged";
         private const string ANIM_PARAM_ISDYING = "_isDying";
@@ -20,13 +30,34 @@ namespace SpellCaller
         {
             _currentLife = _startLife;
             _animator = GetComponent<Animator>();
+
+            _renderer = GetComponentInChildren<Renderer>();
+            if (_renderer != null)
+                _originalColor = _renderer.material.color;
         }
 
         public void ApplyDamage(int damageValue, Vector3 positionValue)
         {
-            _currentLife -= damageValue;
+            if (_isDead) return;
 
+            _currentLife -= damageValue;
             _animator.SetTrigger(ANIM_PARAM_DAMAGED);
+
+            transform.DOKill(true);
+            transform.DORotate(
+                new Vector3(0f, _rotationAngle, 0f),
+                _rotationDuration,
+                RotateMode.LocalAxisAdd
+            )
+            .SetEase(Ease.OutBack);
+
+            if (_renderer != null)
+            {
+                _renderer.material.DOColor(_damageColor, _colorFlashDuration)
+                    .OnComplete(() =>
+                        _renderer.material.DOColor(_originalColor, _colorFlashDuration)
+                    );
+            }
 
             if (_currentLife <= 0)
                 Die();
@@ -34,27 +65,28 @@ namespace SpellCaller
 
         public void ApplyContinuosDamage(int damageValue, float intervalValue, Vector3 positionValue)
         {
-            if (!_canApplyContinuosDamage) return;
-
-            ApplyDamage(damageValue, positionValue);
+            /*
+            if (!_canApplyContinuosDamage || _isDead) return;
 
             _canApplyContinuosDamage = false;
-
+            ApplyDamage(damageValue, positionValue);
             StartCoroutine(ResetCanApplyContinuosDamage_Coroutine(intervalValue));
+            */
         }
 
         private IEnumerator ResetCanApplyContinuosDamage_Coroutine(float intervalValue)
         {
             yield return new WaitForSeconds(intervalValue);
-
             _canApplyContinuosDamage = true;
         }
 
-        // TODO: Fazer algum polimento / juiceness
         public void ForceDie() => Die();
 
         public void Die()
         {
+            if (_isDead) return;
+            _isDead = true;
+
             _animator.SetTrigger(ANIM_PARAM_ISDYING);
             Destroy(gameObject, 3f);
         }
